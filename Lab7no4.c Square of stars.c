2026c710068b472f278f61925f0b14218bff66fe
6804062612323-Thanapat <s6804062612323@email.kmutnb.ac.th>
2026-02-16 21:48:50 +0700

    Update Lab7no4.c Square of stars.c

--- Lab7no4.c Square of stars.c
+++ Lab7no4.c Square of stars.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-void printHead(int width){
-	for(int i=0;i<width;i++){
-		printf("* ");
-	}
-	printf("\n");
-}
 
-void printBody(int width,int height){
-	for(int i=0;i<height-2;i++){
-		printf("* ");
-		for(int j=0;j<width-2;j++){
-			printf("  ");
-		}
-		printf("* ");
-		printf("\n");
-	}
+void printHead(int w) {
+    for (int i = 0; i < w ;i++) {
+        printf("* ");
+    }
+    printf("\n");
 }
-
+void printBody(int w,int h) {
+    for (int i = 0; i < h-2 ; i++) {
+        printf("* ");
+        for (int j = 0; j < (w-2); j++) {
+            printf("  ");
+        }
+        printf("* ");
+        printf("\n");
+    }
+}
 int main() {
-	int width,height;
+	int width, height;
 	scanf("%d %d",&width,&height);
 	printHead(width);
 	printBody(width,height);
